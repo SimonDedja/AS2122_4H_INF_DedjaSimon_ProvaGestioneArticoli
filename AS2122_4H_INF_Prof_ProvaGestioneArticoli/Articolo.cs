ea68ae409b9e6482fa7a2a54604771fe7bf2b1f8
SimonDedja <simon.dedja.15@gmail.com>
2022-03-28 09:10:40 +0200

    Aggiunte property per Articolo

--- AS2122_4H_INF_Prof_ProvaGestioneArticoli/Articolo.cs
+++ AS2122_4H_INF_Prof_ProvaGestioneArticoli/Articolo.cs
@@ -13,8 +13,10 @@ namespace AS2122_4H_INF_Prof_ProvaGestioneArticoli
         string unitaMisura;
         double prezzo;
 
-        // TODO: (9) aggiungere property di sola lettura dei dati necessari all'esterno di Articolo
-        // ...
+        public int Codice { get => codice; }
+        public string Descrizione { get => descrizione; }
+        public string UnitaMisura { get => unitaMisura; }
+        public double Prezzo { get => prezzo; }
 
         public Articolo(int codice, string descrizione, string unitaMisura, double prezzo)
         {
